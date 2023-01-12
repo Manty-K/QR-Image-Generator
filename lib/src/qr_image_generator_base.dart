f6459a1f6c91828fc7d6234298fb1a65a47b744e
Manthan Khandale <manthan@kisan.net>
2023-01-12 18:10:52 +0530

    bug fixes

--- lib/src/qr_image_generator_base.dart
+++ lib/src/qr_image_generator_base.dart
@@ -1,39 +1,53 @@
 import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 import 'package:qr/qr.dart';
 import 'package:image/image.dart' as img;
-import 'package:path/path.dart' as p;
 
 class QRGenerator {
   late String _selectedData;
 
-  late List<List<bool?>> _imageData;
+  late List<List<bool>> _imageData;
 
   late String _outputFilePath;
 
   late int _scale;
 
-  late String _rootFolderPath;
+  String get _miniPath {
+    final splitted = _outputFilePath.split('/');
 
-  final _initialImageName = 'miniQR.png';
+    final filename = splitted.removeLast();
 
-  late String _initialImagePath;
+    final newFilename = 'minified_$filename';
 
-  Future<void> generate({
+    splitted.add(newFilename);
+
+    final minifiedpath = splitted.join('/');
+
+    return minifiedpath;
+  }
+
+  Future<String> generate({
     required String data,
     required String filePath,
     int scale = 5,
   }) async {
+    if (data.trim().isEmpty) {
+      throw 'Data should not be empty';
+    }
+
+    if (filePath.endsWith('.png')) {
+      throw 'File should end with .png';
+    }
+
     _selectedData = data;
     _outputFilePath = filePath;
     _scale = scale;
 
-    final dir = await getTemporaryDirectory();
-    _rootFolderPath = dir.path;
-
-    _initialImagePath = p.join(_rootFolderPath, _initialImageName);
-
-    await _makeImage();
+    try {
+      await _makeImage();
+      return filePath;
+    } catch (e) {
+      throw 'Generate Image Error';
+    }
   }
 
   Future<void> _makeImage() async {
@@ -41,14 +55,14 @@ class QRGenerator {
 
     final qrImage = QrImage(qr);
 
-    _imageData = qrImage.qrModules;
+    _imageData = _getModules(qrImage);
 
     await _makeMiniImage();
 
     await _enlarge();
   }
 
-  _makeMiniImage() async {
+  Future<void> _makeMiniImage() async {
     final data = _imageData;
 
     final image = img.Image(
@@ -60,13 +74,12 @@ class QRGenerator {
       final row = data[i];
       for (int j = 0; j < row.length; j++) {
         final d = row[j];
-        if (d == null) {
-          continue;
-        }
 
+        /// Background Color
         img.Color c = img.ColorRgb8(255, 255, 255);
 
         if (d) {
+          /// Foreground Color
           c = img.ColorRgb8(0, 0, 0);
         }
         image.setPixel(i, j, c);
@@ -74,11 +87,11 @@ class QRGenerator {
     }
 
     final png = img.encodePng(image);
-    await File(_initialImagePath).writeAsBytes(png);
+    await File(_miniPath).writeAsBytes(png);
   }
 
   Future _enlarge() async {
-    final imagePath = _initialImageName;
+    final imagePath = _miniPath;
     final cmd = img.Command()
       ..decodeImageFile(imagePath)
       ..copyResize(width: _imageData.length * _scale)
@@ -90,6 +103,28 @@ class QRGenerator {
   }
 
   Future<void> _deleteMini() async {
-    await File(_initialImageName).delete();
+    await File(_miniPath).delete();
+  }
+
+  List<List<bool>> _getModules(QrImage image) {
+    List<List<bool>> qrdata = [];
+
+    /// Add empty lists
+    for (int i = 0; i < image.moduleCount; i++) {
+      qrdata.add([]);
+    }
+
+    /// Add Data
+    for (var x = 0; x < image.moduleCount; x++) {
+      for (var y = 0; y < image.moduleCount; y++) {
+        if (image.isDark(y, x)) {
+          // render a dark square on the canvas
+          qrdata[x].add(true);
+        } else {
+          qrdata[x].add(false);
+        }
+      }
+    }
+    return qrdata;
   }
 }
