2b21b73622efc922fab153944e425a07cc9125c9
Manthan Khandale <manthan@kisan.net>
2023-01-13 12:41:06 +0530

    Example updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -66,6 +66,8 @@ class _MyHomePageState extends State<MyHomePage> {
       data: 'Hello World!',
       filePath: '$outputDir/hello.png',
       scale: 10,
+      foregroundColor: Colors.yellow,
+      backgroundColor: Colors.blue,
     );
   }
 }
